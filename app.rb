944f5d35270935f494e66df00e98b532d0db9d91
Margarita Potylitsyna <148562840+Ritumka@users.noreply.github.com>
2024-07-12 10:05:48 +0000

    first

--- app.rb
+++ app.rb
@@ -1,6 +1,36 @@
 require "sinatra"
 require "sinatra/reloader"
 
+
+get("/square/new") do
+  erb(:new_square_calc)
+
+end
+
+get '/square/results' do
+@the_num = params.fetch("user_number").to_f
+
+@the_result = @the_num **2
+
+  erb(:square_results)
+end
+
+get("/square_root/new") do
+  erb(:square_root)
+
+end
+
+get("/square_root/results") do
+  @the_num = params.fetch("user_number").to_f
+
+@the_result = Math.sqrt(@the_num)
+
+  erb(:square_root_results)
+
+end
+
+
+
 get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>
