fa630a9ba145e427e38cdb4c2aa828600c5addb8
Margarita Potylitsyna <148562840+Ritumka@users.noreply.github.com>
2024-07-12 10:20:06 +0000

    almost

--- app.rb
+++ app.rb
@@ -47,6 +47,10 @@ get("/random/new") do
 end
 
 get("/random/results") do
+  @min = params.fetch("user_min").to_f
+  @max = params.fetch("user_max").to_f
+
+  @random_number =rand(@min..@max)
   
   erb(:random_results)
 
